f8919de705cf8c6c2b7ca1a9a054e8fc88e2d66b
NikhilMankapure <122514417+NikhilMankapure@users.noreply.github.com>
2023-01-13 16:00:18 +0530

    Jupyter Notebook Assignment
    Assignment for evaluation

---
+++ Jupyter Notebook Assignment.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Nikhil Kuber Mankapure__
+# 
+# Junior Utility Engineer
+
+# *I am interested in data science because it lets me learn the skills those i wish to utilize to model my own set of algorithms to traslate the businesses*
+
+# ### My code is suppose to show a small mathematical calculation for sample code
+
+# In[3]:
+
+
+a = 2
+b = 5
+c = a + b
+print(c)
+
+
+# Horizontal Rule assignment
+# ___
+
+# ~~strike of the line assignment~~
+
+# - Main bullet point
+#      - Sub bullet point
+
+# 1. Numbered item 1
+# 1. Numbered item 2
+
+# In[ ]:
+
+
+
+
